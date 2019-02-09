0056cad3add3c3e382d0b8d49ff2ea2e0a2d4b75
Egor Kolyshkin <egor.kolyshkin@mail.ru>
2019-02-09 21:51:45 +0300

    light improvements

--- main.scm
+++ main.scm
@@ -1,3 +1,16 @@
+
+;nth fibanachi number
 (fib 10)
+
+
 (abs 0)
+
+; Count sum of squares for the two max elems
 (max-square-sum 1 2 3)
+
+;NewTon sqrt iterations
+(sqrt 4)
+
+(sqrt 2)
+
+(sqrt 143)
